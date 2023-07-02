d9dc18067786ac39d12f75a5ffff9f5ac5039702
jose luis <joseluisrios999@gmail.com>
2023-07-02 00:26:04 -0500

    ultimos cambios al dia de hoy

--- juego/Login/src/login/Login1Controller.java
+++ juego/Login/src/login/Login1Controller.java
@@ -10,7 +10,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -33,5 +37,31 @@ public class Login1Controller implements Initializable {
     @FXML
     private void labSalir (ActionEvent event) {
     System.exit(0);
+    }
+    @FXML
+     private TextField txtuser;
+    @FXML
+     private Button btnlogin;
+ 
+    @FXML // metodo para que no se puedan ingresar espacios en blanco en el nombre
+    private void eventKey(KeyEvent event){
+        Object evt = event.getSource();
+        
+        if (event.equals(txtuser)){
+            if(event.getCharacter().equals(""))
+                event.consume();
+        }
+    }
+    @FXML
+    private void eventAction(ActionEvent event){
+        Object evt = event.getSource();
+        if(evt.equals(btnlogin)){
+        if (txtuser.getText().isEmpty()){
+            String user = txtuser.getText();
+        }else {
+            JOptionPane.showMessageDialog(null, "Escribe el nombre");
+        }
+       }
+        
     }
 }
